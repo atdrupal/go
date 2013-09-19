e76e3d29becf3cf1cc52a67d7edbc0b9aa0b8262
Andy Truong <thehongtt@gmail.com>
2013-09-20 00:13:27 +0700

    List more options in go.config.sample.php

--- go.config.sample.php
+++ go.config.sample.php
@@ -6,6 +6,30 @@
  */
 
 // #####################
+// Disable autoload feature
+// #####################
+define('GO_DISABLE_AUTOLOAD', TRUE);
+
+// #####################
+// Simple Google Analytics
+// #####################
+define('GO_GOOGLE_ANALYTICS', 'UA-12345');
+
+// #####################
+// Simple 403/404
+//
+//  403 => Redirect to Login page
+//  404 => Redirect to Search page
+// #####################
+define('GO_403', 1);
+define('GO_404', 'search/content');
+
+// #####################
+// Keep /node page, do not redirect to front page
+// #####################
+define('GO_SKIP_NODE_TO_FRONT', 1);
+
+// #####################
 // Select default format for formatted text fields.
 //
 // 0, 1, 2 is ID of user role.
