e3a61d5740953c4100a5652af26098d5b755a15a
Andy Truong <thehongtt@gmail.com>
2013-11-17 16:51:21 +0700

    * use at_valid() instead of go_enabled().

--- lib/Hook/Init.php
+++ lib/Hook/Init.php
@@ -3,11 +3,11 @@ namespace Drupal\go\Hook;
 
 class Init {
   public function execute() {
-    if (go_enabled('go_google_analytics')) {
+    if (at_valid('go_google_analytics')) {
       return at_id(new \Drupal\go\Misc\SEO\GoogleAnalytics())->execute();
     }
 
-    if (go_enabled('go_skip_node_to_front')) {
+    if (at_valid('go_skip_node_to_front')) {
       return at_id(new \Drupal\go\Misc\SEO\RedirectNodeToFront())->execute();
     }
   }
